005a27f1203d742204d96f5b47c8bacd7da1ad7c
Juan Daniel Rodríguez Espinoza <drany@live.com>
2020-12-17 04:16:07 -0600

    Método post listo para probar el backend

--- src/app/datos.service.ts
+++ src/app/datos.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { CookieService } from 'ngx-cookie-service';
+import { Producto } from './productos/productos.component';
 
 const URL: string = 'http://localhost/bazar-rest/';
 
@@ -8,9 +9,28 @@ const URL: string = 'http://localhost/bazar-rest/';
   providedIn: 'root',
 })
 export class DatosService {
-  
   constructor(private http: HttpClient, private galleta: CookieService) {}
   private cuenta = { user: '', token: '' };
+
+  editarProducto(producto: Producto) {
+    let headers = new HttpHeaders();
+    headers.append('Authorization', this.cuenta.token);
+
+    if (producto.id == 0) {
+      let formData = new FormData();
+
+      formData.append('id', producto.id + '');
+      formData.append('nombre', producto.nombre);
+      formData.append('precioCompra', producto.precioCompra + '');
+      formData.append('precioVenta', producto.precioVenta + '');
+      if (producto.imagen)
+        formData.append('imagen', producto.imagen, producto.imagen.name);
+
+        return this.http.post(URL + 'productos.php', formData,{headers: headers});
+      } else {
+        return this.http.post(URL + 'error.php',{headers: headers});
+    }
+  }
 
   login(user: string, pass: string) {
     let Params = new HttpParams();
@@ -27,7 +47,7 @@ export class DatosService {
     this.galleta.set('token', token);
   }
 
-  getCuenta(){
+  getCuenta() {
     this.cuenta.user = this.galleta.get('user');
     this.cuenta.token = this.galleta.get('token');
 
--- src/app/productos/productos.component.ts
+++ src/app/productos/productos.component.ts
@@ -1,20 +1,88 @@
 import { Component, OnInit } from '@angular/core';
-
-
+import { PlatformLocation } from '@angular/common';
+import { DatosService } from '../datos.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-productos',
   templateUrl: './productos.component.html',
-  styleUrls: ['./productos.component.css']
+  styleUrls: ['./productos.component.css'],
 })
 export class ProductosComponent implements OnInit {
+  constructor(
+    private plataformLocation: PlatformLocation,
+    private datos: DatosService,
+    private toast: ToastrService
+  ) {}
 
-  
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  products: string[] = ['Ajo', 'Bistec', 'Chorizo', 'Durazno', 'E'];
+
+  productoSeleccionado: Producto = {
+    id: 0,
+    nombre: '',
+    precioCompra: 0,
+    precioVenta: 0,
+    venta: null,
+    imagen: undefined,
+  };
+  imgEditar: string = '../../assets/ic_add.png';
+
+  onFileSelected(event: any) {
+    console.log(event);
+
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imgEditar = reader.result as string;
+    };
+
+    let file: File = event.target.files[0];
+    this.productoSeleccionado.imagen = file;
+
+    reader.readAsDataURL(file);
   }
 
-products: string[] =[];
+  limpiarProductoSeleccionado() {
+    this.productoSeleccionado = {
+      id: 0,
+      nombre: '',
+      precioCompra: 0,
+      precioVenta: 0,
+      venta: null,
+      imagen: undefined,
+    };
+    this.imgEditar = '../../assets/ic_add.png';
+  }
 
+  editarProducto() {
+    let correcto = true;
+    if (this.productoSeleccionado.imagen == undefined) {
+      this.toast.error('No has seleccionado una imagen');
+      correcto = false;
+    }
+    if (this.productoSeleccionado.nombre == '') {
+      this.toast.error('Debes poner un nombre a tu producto');
+      correcto = false;
+    }
+    if (this.productoSeleccionado.precioCompra <= 0) {
+      this.toast.error('Debes poner un precio de compra');
+      correcto = false;
+    }
+    if (this.productoSeleccionado.precioVenta <= 0) {
+      this.toast.error('Debes poner un precio de venta');
+      correcto = false;
+    }
+
+    if (correcto) this.datos.editarProducto(this.productoSeleccionado);
+  }
+}
+
+export interface Producto {
+  id: number;
+  nombre: string;
+  precioCompra: number;
+  precioVenta: number;
+  imagen?: File;
+  venta: any;
 }
